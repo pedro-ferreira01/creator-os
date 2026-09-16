e3c3a52955ae4d92bef00495bc883f4d7c6429d4
Pain <p.augusto7733@gmail.com>
2026-09-16 13:28:37 +0000

    refactor(workspace): remove legacy projects module

--- apps/web/modules/projects/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export * from "./data";
-
-export { default as ProjectCard } from "./components/ProjectCard";
-export { default as ProjectList } from "./components/ProjectList";
